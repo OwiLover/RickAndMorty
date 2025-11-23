973f6bd1b8f6856bfa912c1570b7e6a7db268c9d
Owi Lover <lazygabriel@yandex.ru>
2025-11-23 15:36:13 +0300

    Initial Commit

---
+++ КшслФтвЬщкен/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  КшслФтвЬщкен
+//
+//  Created by Owi Lover on 11/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ КшслФтвЬщкен/____________App.swift
@@ -0,0 +1,17 @@
+//
+//  ____________App.swift
+//  КшслФтвЬщкен
+//
+//  Created by Owi Lover on 11/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ____________App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
